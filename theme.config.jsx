c7ba31f3b27d3e893ea1695d4ff786ada7fe154e
Neodevils <neoaichan@gmail.com>
2023-02-04 02:34:46 +0300

    Quick build with Next.js

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,37 @@
-const withNextra = require("nextra")({
-    theme: "nextra-theme-docs",
-    themeConfig: "./theme.config.jsx",
-});
-
-module.exports = withNextra();
+export default {
+	banner: {
+		key: "v1.2.0",
+		text: (
+			<a
+				href="https://github.com/USERSATOSHI/parsers/releases/tag/v1.2.0"
+				target="_blank"
+			>
+				🎉 aoi.parser v1.2.0 is released. Read more →
+			</a>
+		),
+	},
+	useNextSeoProps() {
+		return {
+			titleTemplate: "%s — aoiparser",
+		};
+	},
+	logo: (
+		<span style={{ fontFamily: "monospace", fontSize: "34px" }}>
+			aoi.parser
+		</span>
+	),
+	project: {
+		link: "https://github.com/Neodevils/aoi.parser",
+	},
+	navigation: {
+		prev: true,
+		next: true,
+	},
+	footer: {
+		text: (
+			<span>
+				{new Date().getFullYear()} © USERSATOSHI. All rights reserved.
+			</span>
+		),
+	},
+};
